a078d2f826b9aacb1b6c1b95d2dcb83dae3f7301
Annabelle Wang <annabel3@andrew.cmu.edu>
2025-09-17 23:12:32 +0000

    fixed smell by reducing complexity of toggleFollow function

--- src/user/follow.js
+++ src/user/follow.js
@@ -16,14 +16,76 @@ module.exports = function (User) {
 		await toggleFollow('unfollow', uid, unfollowuid);
 	};
 
-	async function toggleFollow(type, uid, theiruid) {
-		if (parseInt(uid, 10) <= 0 || parseInt(theiruid, 10) <= 0) {
+	/* beginning of GenAI assisted code to make toggleFollow less complex */
+	// helpers
+	function toUid(value) {
+		const n = Number.parseInt(value, 10);
+		if (!Number.isInteger(n) || n <= 0) {
 			throw new Error('[[error:invalid-uid]]');
 		}
+		return n;
+	}
+	
+	function assertValidTransition(shouldFollow, isFollowing) {
+		if (shouldFollow && isFollowing) {
+			throw new Error('[[error:already-following]]');
+		}
+		if (!shouldFollow && !isFollowing) {
+			throw new Error('[[error:not-following]]');
+		}
+	}
+ 
+	async function applyFollowAction(shouldFollow, uid, theiruid) {
+		const followingKey = `following:${uid}`;
+		const followersKey = `followers:${theiruid}`;
+   
+		if (shouldFollow) {
+			const now = Date.now();
+			await db.sortedSetAddBulk([
+				[followingKey, now, theiruid],
+				[followersKey, now, uid],
+			]);
+			return;
+		}
+   
+		await db.sortedSetRemoveBulk([
+			[followingKey, theiruid],
+			[followersKey, uid],
+		]);
+	}
+ 
 
-		if (parseInt(uid, 10) === parseInt(theiruid, 10)) {
+
+	async function updateFollowCounts(uid, theiruid) {
+		const [followingCount, followingRemoteCount, followerCount, followerRemoteCount] =
+		await db.sortedSetsCard([
+			`following:${uid}`,
+			`followingRemote:${uid}`,
+			`followers:${theiruid}`,
+			`followersRemote:${theiruid}`,
+		]);
+   
+		await Promise.all([
+			User.setUserField(uid, 'followingCount', followingCount + followingRemoteCount),
+			User.setUserField(theiruid, 'followerCount', followerCount + followerRemoteCount),
+		]);
+	}
+ 
+	// main
+	async function toggleFollow(type, uid, theiruid) {
+		console.log('Annabelle');
+		const shouldFollow = type === 'follow';
+		if (!shouldFollow && type !== 'unfollow') {
+			throw new Error('[[error:invalid-operation]]');
+		}
+	
+		// validate uids (also prevents self-follow)
+		const myUid = toUid(uid);
+		const otherUid = toUid(theiruid);
+		if (myUid === otherUid) {
 			throw new Error('[[error:you-cant-follow-yourself]]');
 		}
+	
 		const [exists, isFollowing] = await Promise.all([
 			User.exists(theiruid),
 			User.isFollowing(uid, theiruid),
@@ -31,41 +93,21 @@ module.exports = function (User) {
 		if (!exists) {
 			throw new Error('[[error:no-user]]');
 		}
-
+	
 		await plugins.hooks.fire('filter:user.toggleFollow', {
 			type,
 			uid,
 			theiruid,
 			isFollowing,
 		});
+	
+		assertValidTransition(shouldFollow, isFollowing);
+		await applyFollowAction(shouldFollow, uid, theiruid);
+		await updateFollowCounts(uid, theiruid);
 
-		if (type === 'follow') {
-			if (isFollowing) {
-				throw new Error('[[error:already-following]]');
-			}
-			const now = Date.now();
-			await db.sortedSetAddBulk([
-				[`following:${uid}`, now, theiruid],
-				[`followers:${theiruid}`, now, uid],
-			]);
-		} else {
-			if (!isFollowing) {
-				throw new Error('[[error:not-following]]');
-			}
-			await db.sortedSetRemoveBulk([
-				[`following:${uid}`, theiruid],
-				[`followers:${theiruid}`, uid],
-			]);
-		}
-
-		const [followingCount, followingRemoteCount, followerCount, followerRemoteCount] = await db.sortedSetsCard([
-			`following:${uid}`, `followingRemote:${uid}`, `followers:${theiruid}`, `followersRemote:${theiruid}`,
-		]);
-		await Promise.all([
-			User.setUserField(uid, 'followingCount', followingCount + followingRemoteCount),
-			User.setUserField(theiruid, 'followerCount', followerCount + followerRemoteCount),
-		]);
 	}
+
+	/* end of GenAI assisted code to make toggleFollow less complex */
 
 	User.getFollowing = async function (uid, start, stop) {
 		return await getFollow(uid, 'following', start, stop);
