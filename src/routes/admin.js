cb80b192918ee6cbf43346ef5c9af7fbde8541c9
Scarlett <chenghu2@andrew.cmu.edu>
2025-09-17 18:57:56 -0400

    src/routes/admin.js apiRoutes function fixed by Scarlett

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -75,7 +75,7 @@ module.exports = function (app, name, middleware, controllers) {
 };
 
 
-function apiRoutes(router, name, middleware, controllers) {
+function apiRoutes({router, name, middleware, controllers}) {
 	router.get(`/api/${name}/config`, middleware.ensureLoggedIn, helpers.tryRoute(controllers.admin.getConfig));
 	router.get(`/api/${name}/users/csv`, middleware.ensureLoggedIn, helpers.tryRoute(controllers.admin.users.getCSV));
 	router.get(`/api/${name}/groups/:groupname/csv`, middleware.ensureLoggedIn, helpers.tryRoute(controllers.admin.groups.getCSV));
