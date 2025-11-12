6938f61f9c08991752da3b25320b14014e5f64ad
alanj268 <alanjose@andrew.cmu.edu>
2025-11-12 04:54:40 +0000

    Fixed deployment, connected docker container to translation micro service

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,25 +5,25 @@
 const translatorApi = module.exports;
 
 // translatorApi.translate = function (postData) {
-// 	return ['is_english',postData];
+// return ['is_english',postData];
 // };
 
 translatorApi.translate = async function (postData) {
 	//  Edit the translator URL below
-	const TRANSLATOR_API = "http://172.17.0.3:5000/"
+	const TRANSLATOR_API = 'http://translation-service:5000/';
 	const encodedContent = encodeURIComponent(postData.content);
 	const url = `${TRANSLATOR_API}?content=${encodedContent}`;
-	console.log("FETCHING: " + url)
+	console.log('FETCHING: ' + url);
 	try {
 		const response = await fetch(url);
 		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`);
+			throw new Error(`Translation API error! status: ${response.status}`);
 		}
 		const data = await response.json();
-		console.log("DATA: " + JSON.stringify(data))
+		console.log('DATA: ' + JSON.stringify(data));
 		return [data.is_english, data.translated_content];
 	} catch (error) {
-		console.error("FETCH ERROR: ", error);
+		console.error('FETCH ERROR: ', error);
 		throw error;
 	}
 };
